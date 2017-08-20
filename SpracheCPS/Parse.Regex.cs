e13bb8fee7ca989380d181af7df76af173cb15be
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2017-08-21 05:02:43 +0900

    RegexMatchParserConsumesInputOnSuccessfulMatch & RegexMatchParserDoesNotConsumeInputOnFailedMatch passed

--- SpracheCPS/Parse.Regex.cs
+++ SpracheCPS/Parse.Regex.cs
@@ -31,78 +31,76 @@ namespace Sprache
         //    return RegexMatch(regex, description).Then(match => Return(match.Value));
         //}
 
-        ///// <summary>
-        ///// Construct a parser from the given regular expression, returning a parser of
-        ///// type <see cref="Match"/>.
-        ///// </summary>
-        ///// <param name="pattern">The regex expression.</param>
-        ///// <param name="description">Description of characters that don't match.</param>
-        ///// <returns>A parser of regex match objects.</returns>
-        //public static Parser<Match> RegexMatch(string pattern, string description = null)
-        //{
-        //    if (pattern == null) throw new ArgumentNullException(nameof(pattern));
+        /// <summary>
+        /// Construct a parser from the given regular expression, returning a parser of
+        /// type <see cref="Match"/>.
+        /// </summary>
+        /// <param name="pattern">The regex expression.</param>
+        /// <param name="description">Description of characters that don't match.</param>
+        /// <returns>A parser of regex match objects.</returns>
+        public static Parser<Match> RegexMatch(string pattern, string description = null)
+        {
+            if (pattern == null) throw new ArgumentNullException(nameof(pattern));
 
-        //    return RegexMatch(new Regex(pattern), description);
-        //}
+            return RegexMatch(new Regex(pattern), description);
+        }
 
-        ///// <summary>
-        ///// Construct a parser from the given regular expression, returning a parser of
-        ///// type <see cref="Match"/>.
-        ///// </summary>
-        ///// <param name="regex">The regex expression.</param>
-        ///// <param name="description">Description of characters that don't match.</param>
-        ///// <returns>A parser of regex match objects.</returns>
-        //public static Parser<Match> RegexMatch(Regex regex, string description = null)
-        //{
-        //    if (regex == null) throw new ArgumentNullException(nameof(regex));
+        /// <summary>
+        /// Construct a parser from the given regular expression, returning a parser of
+        /// type <see cref="Match"/>.
+        /// </summary>
+        /// <param name="regex">The regex expression.</param>
+        /// <param name="description">Description of characters that don't match.</param>
+        /// <returns>A parser of regex match objects.</returns>
+        public static Parser<Match> RegexMatch(Regex regex, string description = null)
+        {
+            if (regex == null) throw new ArgumentNullException(nameof(regex));
 
-        //    regex = OptimizeRegex(regex);
+            regex = OptimizeRegex(regex);
 
-        //    var expectations = description == null
-        //        ? new string[0]
-        //        : new[] { description };
+            var expectations = description == null
+                ? new string[0]
+                : new[] { description };
 
-        //    return i =>
-        //    {
-        //        if (!i.AtEnd)
-        //        {
-        //            var remainder = i;
-        //            var input = i.Source.Substring(i.Position);
-        //            var match = regex.Match(input);
+            return (input, onSuccess, onFailure) =>
+            {
+                if (input.AtEnd)
+                    return onFailure(input, "Unexpected end of input", expectations);
 
-        //            if (match.Success)
-        //            {
-        //                for (int j = 0; j < match.Length; j++)
-        //                    remainder = remainder.Advance();
+                var remainder = input;
+                var input_ = input.Source.Substring(input.Position);
+                var match = regex.Match(input_);
 
-        //                return Result.Success(match, remainder);
-        //            }
+                if (match.Success)
+                {
+                    for (int j = 0; j < match.Length; j++)
+                        remainder = remainder.Advance();
 
-        //            var found = match.Index == input.Length
-        //                            ? "end of source"
-        //                            : string.Format("`{0}'", input[match.Index]);
-        //            return Result.Failure<Match>(
-        //                remainder,
-        //                "string matching regex `" + regex + "' expected but " + found + " found",
-        //                expectations);
-        //        }
+                    return onSuccess(match, remainder);
+                }
 
-        //        return Result.Failure<Match>(i, "Unexpected end of input", expectations);
-        //    };
-        //}
+                var found = match.Index == input_.Length
+                                ? "end of source"
+                                : string.Format("`{0}'", input_[match.Index]);
+                return onFailure(
+                    remainder,
+                    "string matching regex `" + regex + "' expected but " + found + " found",
+                    expectations);
+            };
+        }
 
-        ///// <summary>
-        ///// Optimize the regex by only matching successfully at the start of the input.
-        ///// Do this by wrapping the whole regex in non-capturing parentheses preceded by
-        /////  a `^'.
-        ///// </summary>
-        ///// <remarks>
-        ///// This method is invoked via reflection in unit tests. If renamed, the tests
-        ///// will need to be modified or they will fail.
-        ///// </remarks>
-        //private static Regex OptimizeRegex(Regex regex)
-        //{
-        //    return new Regex(string.Format("^(?:{0})", regex), regex.Options);
-        //}
+        /// <summary>
+        /// Optimize the regex by only matching successfully at the start of the input.
+        /// Do this by wrapping the whole regex in non-capturing parentheses preceded by
+        ///  a `^'.
+        /// </summary>
+        /// <remarks>
+        /// This method is invoked via reflection in unit tests. If renamed, the tests
+        /// will need to be modified or they will fail.
+        /// </remarks>
+        private static Regex OptimizeRegex(Regex regex)
+        {
+            return new Regex(string.Format("^(?:{0})", regex), regex.Options);
+        }
     }
 }
--- Test/ParseTests.cs
+++ Test/ParseTests.cs
@@ -258,25 +258,25 @@ namespace Sprache.Tests
         //    Assert.Equal(0, r.Remainder.Position);
         //}
 
-        //[Fact]
-        //public void RegexMatchParserConsumesInputOnSuccessfulMatch()
-        //{
-        //    var digits = Parse.RegexMatch(@"\d(\d*)");
-        //    var r = digits.TryParse("123d");
-        //    Assert.True(r.WasSuccessful);
-        //    Assert.Equal("123", r.Value.Value);
-        //    Assert.Equal("23", r.Value.Groups[1].Value);
-        //    Assert.Equal(3, r.Remainder.Position);
-        //}
+        [Fact]
+        public void RegexMatchParserConsumesInputOnSuccessfulMatch()
+        {
+            var digits = Parse.RegexMatch(@"\d(\d*)");
+            var r = digits.TryParse("123d");
+            Assert.True(r.WasSuccessful);
+            Assert.Equal("123", r.Value.Value);
+            Assert.Equal("23", r.Value.Groups[1].Value);
+            Assert.Equal(3, r.Remainder.Position);
+        }
 
-        //[Fact]
-        //public void RegexMatchParserDoesNotConsumeInputOnFailedMatch()
-        //{
-        //    var digits = Parse.RegexMatch(@"\d+");
-        //    var r = digits.TryParse("d123");
-        //    Assert.False(r.WasSuccessful);
-        //    Assert.Equal(0, r.Remainder.Position);
-        //}
+        [Fact]
+        public void RegexMatchParserDoesNotConsumeInputOnFailedMatch()
+        {
+            var digits = Parse.RegexMatch(@"\d+");
+            var r = digits.TryParse("d123");
+            Assert.False(r.WasSuccessful);
+            Assert.Equal(0, r.Remainder.Position);
+        }
 
         //[Fact]
         //public void PositionedParser()
