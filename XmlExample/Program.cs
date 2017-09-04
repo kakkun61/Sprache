318486669e82cc320cd7e042fe25e4be05dc3d34
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2017-09-04 09:19:22 +0900

    use BenchmarkDotNet

--- XmlExample/Program.cs
+++ XmlExample/Program.cs
@@ -1,12 +1,15 @@
 ﻿extern alias sprache_current;
 extern alias sprache_c2cf535;
-
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.IO;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
 using SpracheCurrent = sprache_current::Sprache;
 using SpracheC2cf535 = sprache_c2cf535::Sprache;
 
@@ -49,60 +52,48 @@ namespace XmlExample
         }
     }
 
+    class BenchmarkConfig : ManualConfig
+    {
+        public BenchmarkConfig()
+        {
+            Add(MarkdownExporter.GitHub);
+            Add(MemoryDiagnoser.Default);
+
+            Add(Job.ShortRun);
+        }
+    }
+
+    [Config(typeof(BenchmarkConfig))]
+    public class XmlParserBenchmark
+    {
+        string input;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            input = File.ReadAllText("TestFile.xml", Encoding.UTF8);
+        }
+
+        [Benchmark(Baseline = true)]
+        public object C2cf535()
+        {
+            return SpracheC2cf535.ParserExtensions.Parse(XmlParserC2cf535.Document, input);
+        }
+
+        [Benchmark]
+        public object Current()
+        {
+            return SpracheCurrent.ParserExtensions.Parse(XmlParserCurrent.Document, input);
+        }
+    }
+
     class Program
     {
         static void Main()
         {
-            MeasurePerformance("c2cf535", () =>
-            {
-                string input = File.ReadAllText("TestFile.xml", Encoding.UTF8);
-                var parsed = SpracheC2cf535.ParserExtensions.Parse(XmlParserC2cf535.Document, input);
-                Console.WriteLine(parsed);
-            });
-            MeasurePerformance("current", () =>
-            {
-                string input = File.ReadAllText("TestFile.xml", Encoding.UTF8);
-                var parsed = SpracheCurrent.ParserExtensions.Parse(XmlParserCurrent.Document, input);
-                Console.WriteLine(parsed);
-            });
-        }
+            var switcher = new BenchmarkSwitcher(new[] { typeof(XmlParserBenchmark) });
 
-        static void MeasurePerformance(string tag, Action action)
-        {
-            var n = 10;
-            var m = 10;
-            var elapsedTimeSpans = new double[n];
-            var usedMemories = new long[n];
-            var currentProcess = Process.GetCurrentProcess();
-
-            for (int i = 0; i < n; i++)
-            {
-                var stopwatch = new Stopwatch();
-                stopwatch.Start();
-
-                for (int j = 0; j < m; j++)
-                {
-                    action();
-                }
-
-                stopwatch.Stop();
-                elapsedTimeSpans[i] = stopwatch.Elapsed.TotalMilliseconds;
-                usedMemories[i] = currentProcess.WorkingSet64;
-
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
-                GC.Collect();
-            }
-
-            {
-                Console.WriteLine("{0}", tag);
-                Console.WriteLine("mean time [ms]: {0}", TimeSpan.FromMilliseconds(elapsedTimeSpans.Average()));
-                Console.WriteLine("max time [ms]: {0}", TimeSpan.FromMilliseconds(elapsedTimeSpans.Max()));
-                Console.WriteLine("min time [ms]: {0}", TimeSpan.FromMilliseconds(elapsedTimeSpans.Min()));
-                Console.WriteLine("mean memory [B]: {0}", usedMemories.Average());
-                Console.WriteLine("max memory [B]: {0}", usedMemories.Max());
-                Console.WriteLine("min memory [B]: {0}", usedMemories.Min());
-            }
+            switcher.Run(new[] { "0" });
         }
     }
 }
